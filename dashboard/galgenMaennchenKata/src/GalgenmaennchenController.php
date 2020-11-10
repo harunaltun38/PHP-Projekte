3f5405ed14977e2240803a356067cc91d1b3a0e5
harunaltun38 <harunaltun38@gmx.de>
2020-11-10 21:27:18 +0100

    Fixt das Autoloading

--- dashboard/galgenMaennchenKata/src/GalgenmaennchenController.php
+++ dashboard/galgenMaennchenKata/src/GalgenmaennchenController.php
@@ -2,7 +2,7 @@
 namespace galgenmaennchen;
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-
+require_once 'C:\xampp2\htdocs\dashboard\galgenMaennchenKata\vendor\autoload.php';
 
 class GalgenmaennchenController
 {
